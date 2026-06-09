f6593bbac9fa2bbe82a133662335f2c385c2b666
Nick-Deg <528469@vistacollege.nl>
2026-06-09 13:37:59 +0200

    .
    .

--- code/php/profiel.php
+++ code/php/profiel.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+
+
+$conn = new mysqli("localhost", "root", "root", "skillsphere-ch11");
+
+// pak user id uit session
+$user_id = $_SESSION['user_id'];
+
+// haal username op uit database
+$sql = "SELECT username FROM users WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$user = $result->fetch_assoc();
+?>
+
+<h1>Welkom, <?php echo $user['username']; ?> 👋</h1>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
