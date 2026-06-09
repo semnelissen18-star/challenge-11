f01d0eea25fa88458e0be20f4565dd79f557fdfb
AykeAmory <529701@vistacollege.nl>
2026-06-09 13:24:29 +0200

    ..
    ...

---
+++ code/php/quiz.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/quiz.css?v=2">
+    <title>Quiz | Skillsphere</title>
+</head>
+<body>
+    
+</body>
+</html> 
+
+<div class="header">
+
+        <div id="logo">
+            <img src="../../image/skillshere-official-logo.png" alt="Logo" width="80" height="80">
+        </div>
+
+        <div class="back-button">
+        <a href="home.php">Terug naar Home</a>
+    </div>
+</div>
+
+<div class="quiz-box">
+    <h1>Quiz</h1>
+    <p>Test je kennis en verdien badges!</p>
+    <a href="quiz-start.php" class="start-quiz-button">Start Quiz</a>
+</div>
