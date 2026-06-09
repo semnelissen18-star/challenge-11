a226877d55d0d02b89996e5b7b297fac495d754c
Nick-Deg <528469@vistacollege.nl>
2026-06-09 13:24:51 +0200

    67
    67

--- code/php/profiel.php
+++ code/php/profiel.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skillsphere</title>
-    <link rel="stylesheet" href="../css/profiel.css?v=1">
+    <link rel="stylesheet" href="../css/profiel.css?v=2">
+    <script src="../js/profiel.js"></script>
 </head>
 
 <body>
@@ -18,6 +19,12 @@
         <div class="search-box">
             <input type="text" placeholder="Zoeken...">
             <span class="icon">🔍</span>
+        </div>
+
+        <div class="box2">
+            <div class="usericon">
+                <img src="../../image/User-Icon-Grey.webp" alt="usericon" width="100" height="100">
+            </div>
         </div>
 
         <div class="nav">
