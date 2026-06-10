2ec685ce9c342fee490e8cbebf82403c0ea227fb
AykeAmory <529701@vistacollege.nl>
2026-06-10 09:31:27 +0200

    ..
    ////

--- code/php/inloggen.php
+++ code/php/inloggen.php
@@ -27,13 +27,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="../css/inloggen.css?v=1">
+    <link rel="stylesheet" href="../css/inloggen.css?v=2">
     <title>Inloggen | Skillsphere</title>
 </head>
 <body>
--- code/php/profiel.php
+++ code/php/profiel.php
@@ -2,7 +2,8 @@
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
-    die("Niet ingelogd");
+    header("Location: inloggen.php?error=niet_ingelogd");
+    exit();
 }
 
 $conn = new mysqli("localhost", "root", "root", "skillsphere-ch11");
@@ -25,7 +26,7 @@ $user = $result->fetch_assoc();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skillsphere</title>
-    <link rel="stylesheet" href="../css/profiel.css?v=3">
+    <link rel="stylesheet" href="../css/profiel.css?v=5">
 </head>
 
 <body>
@@ -43,26 +44,43 @@ $user = $result->fetch_assoc();
 
         <div class="nav">
             <a href="home.php">Home</a>
-            <a href="profile.php">Skills</a>
+            <a href="profile.php">Quiz</a>
             <a href="settings.php">Challenges</a>
             <a href="logout.php">Profiel</a>
             <a href="inloggen.php">Login</a>
-            <a href="inloggen.php">Logout</a>
         </div>
 
     </div>
 
-    <div class="box2">
-          <div class="usericon">
-            <img src="../../image/User-Icon-Grey.webp" alt="User Icon" width="150" height="150">
-        </div>
-          <h1>Welkom, <?php echo $user['username']; ?> 👋<br> Opleiding: <?php echo $user['opleiding']; ?></h1>
-          </div>
+<div class="box2">
+    <div class="usericon">
+        <img src="../../image/User-Icon-Grey.webp" alt="User Icon" width="150" height="150">
+    </div>
 
+    <h1>
+        Welkom, <?php echo $user['username']; ?> 👋
+        <br>
+        Opleiding: <?php echo $user['opleiding']; ?>
+    </h1>
 
-    
+   <div class="badges-container">
 
+    <div class="badges">HTML</div>
 
+    <div class="badges">CSS</div>
+
+    <div class="badges">PHP</div>
+
+   <div class="under-badges-container">
+
+    <div class="under-badges"></div>
+
+    <div class="under-badges"></div>
+
+    <div class="under-badges"></div>
+
+</div>
+        
 </body>
 </html>    
 
